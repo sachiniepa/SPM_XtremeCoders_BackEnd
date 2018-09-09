ee87292639c9a75577d0eeaed1b469fa0d46cc6e
Spencer Fern <USpenSt@PEROOT.COM>
2018-09-09 09:24:11 +0530

    Change db.config file

--- DBconfig/db.config.js
+++ DBconfig/db.config.js
@@ -7,7 +7,7 @@ var StudentSchema = require('../Models/StudentSchema');
 mongoose.model('Student', StudentSchema);
 
 //Connecting to db
-mongoose.connect('mongodb://localhost:27017/Internships', function (err) {
+mongoose.connect('mongodb://admin:root123@ds249992.mlab.com:49992/internship', function (err) {
     if(err){
         console.log(err);
         process.exit(-1);
